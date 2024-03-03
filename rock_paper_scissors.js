7aef14dd97c9862fc76fdc96cec5baed9eb3c11a
Athul <s3958556@student.rmit.edu.au>
2024-03-03 23:53:09 +1100

    Added playGame() function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,35 +1,66 @@
 let hand = ["rock", "paper", "scissors"];
+let userScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return hand[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     }
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        userScore++;
         return "You win! Rock beats Scissors"
     }
     if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You lose! Paper beats Rock"
     }
     if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You lose! Scissors beats Paper"
     }
     if (playerSelection === "paper" && computerSelection === "rock") {
+        userScore++;
         return "You win! Paper beats Rock"
     }
     if (playerSelection === "scissors" && computerSelection === "paper") {
+        userScore++;
         return "You win! Scissors beats Paper"
     }
     if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You lose! Rock beats Scissors"
     }
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-// console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// // console.log(computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
 
+function playGame(){
+    let playerSelection;
+    let computerSelection;
+    for (let i = 1; i < 6; i++) {
+        playerSelection = prompt("Rock, Paper, or Scissors?:")
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = getComputerChoice();
+        console.log("Round = " + i);
+        console.log("Your choice: " + playerSelection + ", Computer's choice: " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your score: " + userScore + ", Computer's score: " + computerScore);
+    }
+    if (userScore > computerScore) {
+        console.log("You won the game!")
+    }
+    else if (computerScore > userScore) {
+        console.log("You lost the game. Good luck next time!")
+    }
+    else {
+        console.log("The game is tied!")
+    }
+
+}
+playGame();
